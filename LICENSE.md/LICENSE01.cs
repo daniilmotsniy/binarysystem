bbb8271ff0a03611ee52ae543a8bc851a1e94b8b
MDaniel <danyamotsniy@gmail.com>
2018-02-10 19:38:30 +0000

    Create LICENSE01.cs

---
+++ LICENSE.md/LICENSE01.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Calculator : Form
+    {
+        public Calculator()
+        {
+            InitializeComponent();
+        }
+
+        int num;
+        Int64 BinaryHolder;
+        char[] BinaryArray;
+        string BinaryResult = "";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_Result_TextChanged(object sender, EventArgs e)
+        {
+            if(textBox_Result.Text == "")
+            {
+                num = 0;
+            }
+            else
+            {
+                num = Int32.Parse(textBox_Result.Text);
+            }
+
+        }
+
+            private void label1_Click(object sender, EventArgs e)
+        {
+            while (num > 0)
+            {
+                BinaryHolder = num % 2;
+                BinaryResult += BinaryHolder;
+                num = num / 2;
+            }
+
+            BinaryArray = BinaryResult.ToCharArray();
+            Array.Reverse(BinaryArray);
+            BinaryResult = new string(BinaryArray);
+            label1.Text += BinaryResult;
+
+        }
+
+        }
+    }
+
